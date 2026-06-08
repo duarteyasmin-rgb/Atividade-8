5846d322dd28ad20854c4a43eef2d825de9a51a8
duarteyasmin-rgb <duarte.yasmin@uni9.edu.br>
2026-06-08 23:22:23 +0000

    Adiciona model Aluno com Data Annotations

---
+++ Models/Aluno.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroAlunos.Models
+{
+    public class Aluno
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string RA { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Curso { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+    }
+}
